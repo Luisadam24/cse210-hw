5fff3f88074a0a613b89e1b6cc37baaeed0c3a1f
Luis Adam <luis.adam24@outlook.com>
2026-09-06 00:35:06 +0200

    Implement guessing game logic in Exercise3 and add number processing in Exercise4

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(0, 100); 
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse(userGuess);
+        while (guess != number)
+        {
+            if (guess > number)
+            { Console.WriteLine("Lower"); }
+            else
+            { Console.WriteLine("Higher"); }
+            Console.Write("What is your guess? ");
+            userGuess = Console.ReadLine();
+            int gue = int.Parse(userGuess);
+            guess = gue;
+
+            
+
+        }
+        if (guess == number)
+        { Console.Write("You guessed it!"); }
+        
+        
+
+
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        string userGuess = Console.ReadLine();
+        int number = int.Parse(userGuess);
+        List<int> numbers;
+        numbers = new List<int>();
+        int total = 0;
+        double average = 0;
+       
+
+
+
+        while (number != 0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter a list of numbers? ");
+            userGuess = Console.ReadLine();
+            number = int.Parse(userGuess);
+       
+
+        }
+        int x = numbers[0];
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.WriteLine(numbers[i]);
+            total = total + numbers[i];
+            if (numbers[i] > x)
+            {
+                x = numbers[i]; 
+            }
+        }
+        average = total / numbers.Count;
+
+
+        Console.WriteLine($"The sum is {total}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {x}");
+
+
     }
 }
